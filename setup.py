411a4fb05adb4b19f0657686e6a8484c4e252460
Geoff Lee <g.lee@ed.ac.uk>
2019-04-09 17:43:50 +0100

    Use pytest-runner for tests

--- setup.py
+++ setup.py
@@ -3,27 +3,6 @@ from codecs import open
 from os import path
 import sys
 
-from setuptools.command.test import test as TestCommand
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main([self.pytest_args])
-        sys.exit(errno)
-
 __version__ = '1.0.0'
 
 here = path.abspath(path.dirname(__file__))
